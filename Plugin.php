558b0276ecc04579d5ed5a975032fa7eb9e5fae3
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-01-26 08:07:04 +0300

    Add plugin description.

---
+++ Plugin.php
@@ -0,0 +1,23 @@
+<?php
+namespace Thewizardplusplus\CopyrightYears;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = ['RainLab.Blog'];
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Copyright Years',
+            'description' =>
+                'Provides the component for calculation and displaying of '
+                . 'copyright years.',
+            'author' => 'thewizardplusplus',
+            'icon' => 'icon-copyright',
+            'homepage' =>
+                'https://github.com/thewizardplusplus/october-copyright-years'
+        ];
+    }
+}
